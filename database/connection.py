096285077fe92988d29a2593cef19b5084fc254c
Dandelion <pen22217@uvg.edu.gt>
2025-09-15 14:10:04 -0600

    Improve docs, add data types and finish DB functions.

--- database/connection.py
+++ database/connection.py
@@ -4,14 +4,14 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-def get_connection():
+def get_connection() -> mariadb.Cursor:
     """Establecer conexión a la base de datos de Ecos del Valle.
     
     Returns:
         cur (mariadb.Cursor): Cursor de la base de datos.
     """
     try:
-        conn = mariadb.connect(
+        conn: mariadb.Connection = mariadb.connect(
             user=os.getenv("DATABASE_USER"),
             password=os.getenv("DATABASE_PASSWORD"),
             host=os.getenv("DATABASE_HOST"),
@@ -26,19 +26,22 @@ def get_connection():
         sys.exit(1)
 
     # Get Cursor
-    cur = conn.cursor()
+    cur: mariadb.Cursor = conn.cursor()
     return cur
 
-def insert_integrante(cursor: mariadb.Cursor, data: tuple):
+def insert_member(cursor: mariadb.Cursor, data: tuple) -> None:
     """Insertar datos en la tabla integrante.
     
     Args:
         cursor (mariadb.Cursor): Cursor de la base de datos.
         data (tuple): Datos a insertar.
+
+    Returns:
+        None
     """
     try:
         cursor.execute(
-            "INSERT INTO integrante (nombre, rol) VALUES (?, ?)", data[0], data[1]
+            "INSERT INTO integrante (nombre, rol) VALUES (?, ?)", (data[0], data[1])
         )
     
         print(f"Integrante {data[0]} agregado correctamente.")
@@ -46,17 +49,20 @@ def insert_integrante(cursor: mariadb.Cursor, data: tuple):
     except mariadb.Error as e:
         print(f"Ocurrió un error al insertar al integrante: {e}")
 
-def insert_instrumento(cursor: mariadb.Cursor, data: tuple):
+def insert_instrument(cursor: mariadb.Cursor, data: tuple) -> None:
     """Insertar datos en la tabla instrumento.
 
     Args:
         cursor (mariadb.Cursor): Cursor de la base de datos.
         data (tuple): Datos a insertar.
+
+    Returns:
+        None
     """
 
     try:
         cursor.execute(
-            "INSERT INTO instrumento (nombre, tipo) VALUES (?,?)", data[0], data[1]
+            "INSERT INTO instrumento (nombre, tipo) VALUES (?,?)", (data[0], data[1])
         )
 
         print(f"Instrumento {data[0]} agregado correctamente.")
@@ -64,8 +70,43 @@ def insert_instrumento(cursor: mariadb.Cursor, data: tuple):
     except mariadb.Error as e:
         print(f"Ocurrió un error al insertar al instrumento: {e}")
 
-def insert_melodia(cursor: mariadb.Cursor, data: tuple):
-    pass
+def insert_melody(cursor: mariadb.Cursor, data: tuple) -> None:
+    """Insertar datos en la tabla melodia.
 
-def assign_instrumento_melodia(cursor: mariadb.Cursor, data: tuple):
-    pass
+    Args:
+        cursor (mariadb.Cursor): Cursor de la base de datos.
+        data (tuple): Datos a insertar.
+
+    Returns:
+        None
+    """
+
+    try:
+        cursor.execute(
+            "INSERT INTO melodia (titulo, genero) VALUES (?,?)", (data[0], data[1])
+        )
+
+        print(f"Melodía {data[0]} agregada correctamente.")
+
+    except mariadb.Error as e:
+        print(f"Ocurrió un error al insertar la melodía: {e}")
+
+def assign_instrument_member_melody(cursor: mariadb.Cursor, data: tuple) -> None:
+    """Asignar un instrumento y una melodía a un integrante.
+
+    Args:
+        cursor (mariadb.Cursor): Cursor de la base de datos.
+        data (tuple): Datos a insertar.
+
+    Returns:
+        None
+    """
+    try:
+        cursor.execute(
+            "INSERT INTO integrante_instrumento_melodia (integrante_id, instrumento_id, melodia_id, puesto) VALUES (?, ?, ?, ?)",  (data[0], data[1], data[2], data[3])
+        )
+
+        print(f"Asignación de instrumento {data[1]} y melodía {data[2]} a integrante {data[0]} con el puesto {data[3]} realizada correctamente.")
+
+    except mariadb.Error as e:
+        print(f"Ocurrió un error al asignar el instrumento y la melodía: {e}")
